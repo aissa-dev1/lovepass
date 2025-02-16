f698017b263c2e9875025c80c5c2a4768dd0c013
aissa-dev1 <aissadev1@gmail.com>
2025-02-16 22:38:39 +0100

    naming love-card to love-pass-card

--- src/components/love-pass-card.tsx
+++ src/components/love-pass-card.tsx
@@ -1,32 +1,27 @@
 import clsx from "clsx";
 
-interface LovePassCardProps {
-  backgroundColor: string;
-  cardTitle: string;
-  cardSubtitle: string;
-  mainText: string;
-  fromText: string;
+export type LovePassCardType = {
+  _id?: string;
+  userAuthToken?: string;
+  to: string;
+  from: string;
+  message: string;
   emoji: string;
-}
+  backgroundColor: string;
+  lovePassId?: string;
+};
 
-export default function LovePassCard({
-  backgroundColor,
-  cardTitle,
-  cardSubtitle,
-  mainText,
-  fromText,
-  emoji,
-}: LovePassCardProps) {
+export function LovePassCard(props: LovePassCardType) {
   const charLimit = 70;
 
   return (
     <div
-      style={{ backgroundColor }}
+      style={{ backgroundColor: props.backgroundColor }}
       className="relative w-96 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl h-48 sm:h-56 md:h-64 rounded-2xl shadow-xl text-white p-4 sm:p-6 border border-black/25"
     >
       <div className="absolute top-2 left-4 sm:top-4 sm:left-6">
-        <p className="text-md sm:text-lg font-semibold">{cardTitle}</p>
-        <p className="text-xs sm:text-sm text-white/80">To {cardSubtitle}</p>
+        <p className="text-md sm:text-lg font-semibold">LovePass</p>
+        <p className="text-xs sm:text-sm text-white/80">To {props.to}</p>
       </div>
       <div className="absolute bottom-4 left-4 sm:bottom-6 sm:left-6">
         <p
@@ -34,19 +29,19 @@ export default function LovePassCard({
             "text-xl sm:text-2xl font-bold break-words max-w-xs pr-2 lg:max-w-sm",
             {
               "max-h-20 overflow-y-auto lg:max-h-28":
-                mainText.length > charLimit,
+                props.message.length > charLimit,
             }
           )}
         >
-          {emoji} {mainText}
+          {props.emoji} {props.message}
         </p>
-        <p className="text-xs sm:text-sm text-white/80">From {fromText}</p>
+        <p className="text-xs sm:text-sm text-white/80">From {props.from}</p>
       </div>
       <div className="absolute top-2 right-4 sm:top-4 sm:right-6 text-2xl sm:text-4xl">
-        {emoji}
+        {props.emoji}
       </div>
       <div className="absolute bottom-2 right-4 sm:bottom-4 sm:right-6 text-xs sm:text-sm font-mono">
-        #LP-XXXXXX
+        {props.lovePassId}
       </div>
     </div>
   );
