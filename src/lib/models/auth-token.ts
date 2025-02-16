63180ee16a5d584cff0f814d1c0733797ba7b225
aissa-dev1 <aissadev1@gmail.com>
2025-02-16 22:40:13 +0100

    create auth token model

---
+++ src/lib/models/auth-token.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { Schema, model, models } from "mongoose";
+
+const authTokenSchema = new Schema(
+  {
+    token: { type: String, required: true, unique: true },
+    fingerprint: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const AuthToken =
+  models.AuthToken || model("AuthToken", authTokenSchema);
